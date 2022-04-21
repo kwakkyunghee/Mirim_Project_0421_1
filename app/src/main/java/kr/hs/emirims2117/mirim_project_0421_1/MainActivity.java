7ecd9170cccf5fba8003e39ec6d51e961da52b73
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-04-21 20:06:34 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirims2117/mirim_project_0421_1/MainActivity.java
@@ -0,0 +1,28 @@
+package kr.hs.emirims2117.mirim_project_0421_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        LinearLayout rootLayout=new LinearLayout(this);
+        rootLayout.setOrientation(LinearLayout.VERTICAL);
+        rootLayout.setBackgroundColor(Color.rgb(0,255,0));
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        setContentView(rootLayout, params);
+
+        Button btn=new Button(this);
+        btn.setText("버튼연습");
+        rootLayout.addView(btn);
+    }
+}
